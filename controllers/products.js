6f6225ac871eea2bbe3373739fabe0e59db68ed8
boydlakers15 <jboyd115@icloud.com>
2023-04-19 17:18:50 +0200

    first

--- controllers/products.js
+++ controllers/products.js
@@ -2,6 +2,7 @@ import db from '../db/index.js';
 import ErrorResponse from '../utils/ErrorResponse.js';
 
 export const getProducts = async (req, res, next) => {
+  
   try {
     const { rowCount, rows } = await db.query('SELECT * FROM products;');
     if (!rowCount) throw new ErrorResponse('No products', 404);
@@ -12,74 +13,26 @@ export const getProducts = async (req, res, next) => {
 };
 
 export const createProduct = async (req, res, next) => {
-  try {
-    const {
-      body: { name, description, stock, unitPrice }
-    } = req;
-    if (!name || !description || !stock || !unitPrice) throw new ErrorResponse(`Name, description, stock and unit price are required`, 400);
-    const {
-      rows: [newProduct]
-    } = await db.query(`INSERT INTO products(name, description, stock, unit_price) VALUES($1, $2, $3, $4) RETURNING *`, [
-      name,
-      description,
-      stock,
-      unitPrice
-    ]);
-    res.status(201).json(newProduct);
+   try {
+    const { rowCount, rows } = await db.query(
+      `INSERT INTO products(name, description, stock, unit_price) VALUES($1, $2, $3, $4) RETURNING *`, 
+      ['iphone14 pro max', 'iphone14 pro max', '200', '1500']
+      );
+    if (!rowCount) throw new ErrorResponse('No products', 404);
+    res.json(rows);
   } catch (error) {
     next(error);
   }
 };
 
-export const getProduct = async (req, res, next) => {
-  try {
-    const {
-      params: { id }
-    } = req;
-    const {
-      rowCount,
-      rows: [product]
-    } = await db.query('SELECT * FROM products WHERE id = $1', [id]);
-    if (!rowCount) throw new ErrorResponse(`Product with id of ${id} does not exist`, 404);
-    res.json(product);
-  } catch (error) {
-    next(error);
-  }
+export const getProduct = (req, res) => {
+  res.json({ msg: 'GET a product' });
 };
 
-export const updateProduct = async (req, res, next) => {
-  try {
-    const {
-      body: { name, description, stock, unitPrice },
-      params: { id }
-    } = req;
-    if (!name || !description || !stock || !unitPrice) throw new ErrorResponse(`Name, description, stock and unit price are required`, 400);
-    const {
-      rowCount,
-      rows: [product]
-    } = await db.query('UPDATE products SET name = $1, description = $2, stock = $3, unit_price = $4 WHERE id = $5 RETURNING *', [
-      name,
-      description,
-      stock,
-      unitPrice,
-      id
-    ]);
-    if (!rowCount) throw new ErrorResponse(`Product with id of ${id} does not exist`, 404);
-    res.json(product);
-  } catch (error) {
-    next(error);
-  }
+export const updateProduct = (req, res) => {
+  res.json({ msg: 'PUT a product' });
 };
 
-export const deleteProduct = async (req, res, next) => {
-  try {
-    const {
-      params: { id }
-    } = req;
-    const { rowCount } = await db.query('DELETE FROM products WHERE id = $1', [id]);
-    if (!rowCount) throw new ErrorResponse(`Product with id of ${id} does not exist`, 404);
-    res.json({ msg: `Product with id of ${id} was deleted` });
-  } catch (error) {
-    next(error);
-  }
+export const deleteProduct = (req, res) => {
+  res.json({ msg: 'DELETE a product' });
 };
--- db/index.js
+++ db/index.js
@@ -1,8 +1,13 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-const pool = new Pool({ connectionString: process.env.PGURI });
+console.log(process.env.PGURI)
 
+const pool = new Pool({ connectionString: process.env.PGURI });
+// pool.query("SELECT NOW()", (err, res) => {
+//   console.log(err, res);
+//   pool.end();
+// })
 const db = {
   query(text, params) {
     return pool.query(text, params);
