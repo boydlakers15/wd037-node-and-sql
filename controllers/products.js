b56a3352729aad25e0008ed01f07f35b25fd1980
Jorge Paul <jserranopaul@gmail.com>
2023-04-19 16:21:26 +0200

    Update and delete plus body validation (basic)

--- controllers/products.js
+++ controllers/products.js
@@ -16,6 +16,7 @@ export const createProduct = async (req, res, next) => {
     const {
       body: { name, description, stock, unitPrice }
     } = req;
+    if (!name || !description || !stock || !unitPrice) throw new ErrorResponse(`Name, description, stock and unit price are required`, 400);
     const {
       rows: [newProduct]
     } = await db.query(`INSERT INTO products(name, description, stock, unit_price) VALUES($1, $2, $3, $4) RETURNING *`, [
@@ -46,10 +47,39 @@ export const getProduct = async (req, res, next) => {
   }
 };
 
-export const updateProduct = (req, res) => {
-  res.json({ msg: 'PUT a product' });
+export const updateProduct = async (req, res, next) => {
+  try {
+    const {
+      body: { name, description, stock, unitPrice },
+      params: { id }
+    } = req;
+    if (!name || !description || !stock || !unitPrice) throw new ErrorResponse(`Name, description, stock and unit price are required`, 400);
+    const {
+      rowCount,
+      rows: [product]
+    } = await db.query('UPDATE products SET name = $1, description = $2, stock = $3, unit_price = $4 WHERE id = $5 RETURNING *', [
+      name,
+      description,
+      stock,
+      unitPrice,
+      id
+    ]);
+    if (!rowCount) throw new ErrorResponse(`Product with id of ${id} does not exist`, 404);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const deleteProduct = (req, res) => {
-  res.json({ msg: 'DELETE a product' });
+export const deleteProduct = async (req, res, next) => {
+  try {
+    const {
+      params: { id }
+    } = req;
+    const { rowCount } = await db.query('DELETE FROM products WHERE id = $1', [id]);
+    if (!rowCount) throw new ErrorResponse(`Product with id of ${id} does not exist`, 404);
+    res.json({ msg: `Product with id of ${id} was deleted` });
+  } catch (error) {
+    next(error);
+  }
 };
